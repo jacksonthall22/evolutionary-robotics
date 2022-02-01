1e6e3017374d2be6c124bfbc7a9a2de39a297148
Jackson Hall <jacksonthall22@gmail.com>
2022-01-31 20:09:26 -0500

    Simplify world

--- generate.py
+++ generate.py
@@ -1,28 +1,11 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF('boxes.sdf')
 
-l = 1
-w = 1
-h = 1
+def main():
+    pyrosim.Start_SDF('world.sdf')
+    pyrosim.Send_Cube(name='Box', pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-x = 0
-y = 0
-z = h/2
 
-x_layers = 5
-y_layers = 5
-z_layers = 15
-
-for x_idx in range(x_layers):
-    for y_idx in range(y_layers):
-        base_height = 0
-        for z_idx in range(z_layers):
-            scale = 0.9 ** z_idx
-            z_loc = base_height + h*scale/2
-            
-            pyrosim.Send_Cube(name='Box', pos=[x_idx*l, y_idx*w, z_loc], size=[l*scale, w*scale, h*scale])
-            
-            base_height += h*scale
-
-pyrosim.End()
+if __name__ == '__main__':
+    main()
--- simulate.py
+++ simulate.py
@@ -2,15 +2,21 @@ import pybullet as p
 import time
 import pybullet_data
 
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0, 0, -9.8)
-planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
 
-for i in range(10000):
-    p.stepSimulation()
-    time.sleep(1/100)
-    print(i)
+def main():
+    physicsClient = p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0, 0, -9.8)
+    planeId = p.loadURDF("plane.urdf")
+    p.loadSDF("world.sdf")
 
-p.disconnect()
+    for i in range(10000):
+        p.stepSimulation()
+        time.sleep(1/100)
+        print(i)
+
+    p.disconnect()
+
+
+if __name__ == '__main__':
+    main()
