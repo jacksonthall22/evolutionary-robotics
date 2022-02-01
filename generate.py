a851a3ebbce457b0bbb08b35a720a007c86f6243
Jackson Hall <jacksonthall22@gmail.com>
2022-01-31 20:39:23 -0500

    Add create_world() and create_robot() with simple "hook" robot

--- generate.py
+++ generate.py
@@ -1,10 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
 
-def main():
+def create_world():
     pyrosim.Start_SDF('world.sdf')
-    pyrosim.Send_Cube(name='Box', pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name='Box', pos=[2, 2, 0.5], size=[1, 1, 1])
     pyrosim.End()
+
+def create_robot():
+    pyrosim.Start_URDF('body.urdf')
+    pyrosim.Send_Cube(name='Link0', pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name='Link0_Link1', parent='Link0', child='Link1', type='revolute', position=[0, 0, 1])
+    pyrosim.Send_Cube(name='Link1', pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name='Link1_Link2', parent='Link1', child='Link2', type='revolute', position=[0, 0, 1])
+    pyrosim.Send_Cube(name='Link2', pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name='Link2_Link3', parent='Link2', child='Link3', type='revolute', position=[0, 0.5, 0.5])
+    pyrosim.Send_Cube(name='Link3', pos=[0, 0.5, 0], size=[1, 1, 1])
+    pyrosim.End()
+
+def main():
+    create_world()
+    create_robot()
 
 
 if __name__ == '__main__':
--- simulate.py
+++ simulate.py
@@ -7,8 +7,9 @@ def main():
     physicsClient = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0, 0, -9.8)
-    planeId = p.loadURDF("plane.urdf")
-    p.loadSDF("world.sdf")
+    planeId = p.loadURDF('plane.urdf')
+    robotId = p.loadURDF('body.urdf')
+    p.loadSDF('world.sdf')
 
     for i in range(10000):
         p.stepSimulation()
