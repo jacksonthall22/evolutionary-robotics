b31f74bc095cc94d576c55a70e8fbb6916fb82b4
Jackson Hall <jacksonthall22@gmail.com>
2022-01-31 19:43:50 -0500

    Added procedurally-generated boxes

--- generate.py
+++ generate.py
@@ -1,14 +1,28 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF('boxes.sdf')
 
-length = 1
-width = 1
-height = 1
+l = 1
+w = 1
+h = 1
 
 x = 0
 y = 0
-z = height/2
+z = h/2
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+x_layers = 5
+y_layers = 5
+z_layers = 15
+
+for x_idx in range(x_layers):
+    for y_idx in range(y_layers):
+        base_height = 0
+        for z_idx in range(z_layers):
+            scale = 0.9 ** z_idx
+            z_loc = base_height + h*scale/2
+            
+            pyrosim.Send_Cube(name='Box', pos=[x_idx*l, y_idx*w, z_loc], size=[l*scale, w*scale, h*scale])
+            
+            base_height += h*scale
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -4,11 +4,11 @@ import pybullet_data
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
-for i in range(1000):
+for i in range(10000):
     p.stepSimulation()
     time.sleep(1/100)
     print(i)
