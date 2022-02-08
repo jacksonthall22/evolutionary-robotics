cd2120772eeffc0a6d06d2618bdb7c2df27db022
Jackson Hall <jacksonthall22@gmail.com>
2022-02-07 23:16:11 -0500

    Finish E. joints

--- generate.py
+++ generate.py
@@ -8,13 +8,11 @@ def create_world():
 
 def create_robot():
     pyrosim.Start_URDF('body.urdf')
-    pyrosim.Send_Cube(name='Link0', pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Joint(name='Link0_Link1', parent='Link0', child='Link1', type='revolute', position=[0, 0, 1])
-    pyrosim.Send_Cube(name='Link1', pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Joint(name='Link1_Link2', parent='Link1', child='Link2', type='revolute', position=[0, 0, 1])
-    pyrosim.Send_Cube(name='Link2', pos=[0, 0, 0.5], size=[1, 1, 1])
-    pyrosim.Send_Joint(name='Link2_Link3', parent='Link2', child='Link3', type='revolute', position=[0, 0.5, 0.5])
-    pyrosim.Send_Cube(name='Link3', pos=[0, 0.5, 0], size=[1, 1, 1])
+    pyrosim.Send_Cube(name='Torso', pos=[0, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name='Backleg', pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name='Frontleg', pos=[0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name='Torso_Backleg', parent='Torso', child='Backleg', type='revolute', position=[-0.5, 0, 1])
+    pyrosim.Send_Joint(name='Torso_Frontleg', parent='Torso', child='Frontleg', type='revolute', position=[0.5, 0, 1])
     pyrosim.End()
 
 def main():
