314eeb0a62f446702e6ed8c320cc036d7946f080
Jackson Hall <jacksonthall22@gmail.com>
2022-02-14 23:46:17 -0500

    Move joints sinusoidally

--- analyze.py
+++ analyze.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load('data/backLegSensorValues.npy')
 frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+target_angles = np.load('data/TARGET_ANGLES.npy')
 
-plt.plot(backLegSensorValues, label='backLeg', linewidth=2)
-plt.plot(frontLegSensorValues, label='frontLeg', linewidth=2)
+# plt.plot(backLegSensorValues, label='backLeg', linewidth=2)
+# plt.plot(frontLegSensorValues, label='frontLeg', linewidth=2)
+plt.plot(target_angles)
 plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time
 import pybullet_data
 from pyrosim import pyrosim
 import numpy as np
+import math
 
 
 def main():
@@ -14,7 +15,14 @@ def main():
     p.loadSDF('world.sdf')
 
     TIME_STEPS = 1000
-    TICKS_PER_SEC = 100
+    TICKS_PER_SEC = 240
+
+    # Joint angles
+    amplitude = math.pi / 4
+    frequency = 10
+    phase_offset = 0
+    TARGET_ANGLES = amplitude * np.sin(frequency * np.linspace(0, 2 * math.pi, TIME_STEPS) + phase_offset)
+    np.save('data/TARGET_ANGLES.npy', TARGET_ANGLES)
 
     backLegSensorValues = np.zeros(TIME_STEPS)
     frontLegSensorValues = np.zeros(TIME_STEPS)
@@ -23,11 +31,17 @@ def main():
         p.stepSimulation()
         backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
         frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
         pyrosim.Set_Motor_For_Joint(bodyIndex=robotId,
                                     jointName='Torso_BackLeg',
                                     controlMode=p.POSITION_CONTROL,
-                                    targetPosition=0.0,
-                                    maxForce=500)
+                                    targetPosition=TARGET_ANGLES[i],
+                                    maxForce=50)
+        pyrosim.Set_Motor_For_Joint(bodyIndex=robotId,
+                                    jointName='Torso_FrontLeg',
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPosition=TARGET_ANGLES[i],
+                                    maxForce=50)
 
         print(backLegSensorValues[i])
         time.sleep(1/TICKS_PER_SEC)
