ce80e5c5c840afb7d3fdcf852cc7a9f588cbdb7f
Jackson Hall <jacksonthall22@gmail.com>
2022-04-14 20:54:22 -0400

    Option to search or show best in search.py

--- search.py
+++ search.py
@@ -1,12 +1,16 @@
 from parallelhillclimber import ParallelHillClimber
 import simulation
 
-phc = ParallelHillClimber()
-phc.evolve()
-# input('Press enter to show the best solution!\n>>> ')
-phc.show_best()
+DO_SEARCH = 1
 
-# s = simulation.Simulation('GUI', 0, brain_filename='best_brain.nndf')
-# s.run()
+if DO_SEARCH:
+    phc = ParallelHillClimber()
+    phc.evolve()
+    input('Press enter to show the best solution!\n>>> ')
+    phc.show_best()
+    phc.save_population()
+else:
+    s = simulation.Simulation('GUI', 0, brain_filename='best_brain.nndf')
+    s.run()
 
 print('done!')
