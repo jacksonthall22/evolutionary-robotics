7488e5f0653ea46041286499b01f1145979f2565
Jackson Hall <jacksonthall22@gmail.com>
2022-04-18 19:31:42 -0400

    Fix formatting

--- pyrosim/urdf.py
+++ pyrosim/urdf.py
@@ -1,13 +1,9 @@
 class URDF:
-
     def __init__(self):
-
         self.depth = 0
 
-    def Save_Start_Tag(self,f):
-
+    def Save_Start_Tag(self, f):
         f.write('<robot name = "robot">\n')
 
-    def Save_End_Tag(self,f):
-
-        f.write("</robot>")
+    def Save_End_Tag(self, f):
+        f.write('</robot>')
